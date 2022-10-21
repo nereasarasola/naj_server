41ce66de02f9e95823803da9c26cf6c94cba26f9
NEREA SARASOLA TOMÉ <nerea.sarasola@ikasle.aeg.eus>
2022-10-21 10:32:42 +0200

    Login function done

---
+++ src/controllers/userController.js
@@ -0,0 +1,44 @@
+const userService = require("../services/userService");
+
+const createNewUser = async (req, res) => {
+    const { idToken, name, email } = req.body;
+    if(!idToken){
+       return res.status(400).send({
+        status: "FAILED",
+        data: {
+          error:
+            "Parameter idToken can not be empty",
+        },
+      });
+    }
+    if (!name || !email) {
+      return res.status(400).send({
+        status: "FAILED",
+        data: {
+          error:
+            "One of the following keys is missing or is empty in request body: 'name', 'mail'",
+        },
+      });
+    }
+    const newUser = {
+      idToken: idToken,
+      name: name,
+      email: email,
+      active: true,
+      role: false,
+    };
+    try {
+      const createdUser = await userService.createNewUser(idToken, newUser);
+      res.send({ status: "OK", data: createdUser });
+    } catch (error) {
+      res.status(error?.status || 500).send({
+        status: "FAILED",
+        message: "Failed making the req: ",
+        data: { error: error?.message || error },
+      });
+    }
+  };
+
+module.exports = {
+    createNewUser
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const userRouter = require("./routes/userRoute");
+
+const mongoose = require ('mongoose');
+const mongodbRoute = 'mongodb+srv://aarquero:123@e4p1.dcxvo3h.mongodb.net/users';
+
+app.use(bodyParser.json());
+
+app.use("/api/users", userRouter);
+
+async function start() {
+    try
+    {
+        await mongoose.connect(mongodbRoute);
+        app.listen(PORT, () => {
+            console.log(`API is listening in port ${PORT}`)
+        });
+        console.log('Conexión con Mongo correcta.');
+    }
+    catch(error) 
+    {
+        console.log(`Error al conectar a la base de datos: ${error}`)
+    }
+}
+
+start();
