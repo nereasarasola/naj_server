0394f3bbc4e3ffc1cb3c344dee401cab70f2f8f0
NEREA SARASOLA TOMÉ <nerea.sarasola@ikasle.aeg.eus>
2022-12-26 16:18:32 +0100

    Tarea CC2022-290 finalizada

--- src/services/sockets/socketEvents.js
+++ src/services/sockets/socketEvents.js
@@ -1,55 +1,10 @@
 const User = require('../userService');
-<<<<<<< HEAD
-
-events = (socket) => {
-
-  //Connect
-  console.log({ Clientsocket: socket.id });
-  /*socket.emit("new_user", socket.id);*/
-
-  //Check the state of the user
-  socket.on('state_acolite', async (data) => {
-    try {
-      const updatedUser = await User.patchUser(data.email, data.state);
-      socket.broadcast.emit('state_acolite', updatedUser);
-    } catch(error) {
-      console.log(error);
-      socket.broadcast.emit('state_acoliteError', error);
-    }
-  });
-
-
-  
-    
-    // socket.on('slider', (data) => {
-    //   console.log(data);
-    //   socket.broadcast.emit('slider', data);
-    // });
-    // TEST BROADCAST
-    // socket.on('test_broadcast', async (data) => {
-    //   try {
-    //     socket.broadcast.emit('test_broadcast', data);
-    //   } catch (error) {
-    //     console.log(error);
-    //     socket.emit('test_broadcastError', error);
-    //   }
-    // });
-  
-    //Enter Crypt
-    // socket.on('crypt-enter', data => {
-    //   try {
-    //     const response = //await al service
-    //     socket.emit('crypt-enter', acolit)
-    //   } catch (error) {n
-        
-    //   }
-    // })
-  
-  // socket.on('disconnect', () => {
-  //   console.log('Client disconnected: ', socket.id);      
-=======
-const {NEW_CONNECTION, NEW_CONNECTION_ERROR, DISCONNECTION, ACOLITE_STATE, ACOLITE_STATE_ERROR} = require('../../constants');
+const Doll = require('../dollService');
+const Piece = require('../pieceService');
+const {NEW_CONNECTION, NEW_CONNECTION_ERROR, DISCONNECTION, ACOLITE_STATE, ACOLITE_STATE_ERROR, MISSION_STATUS, MISSION_STATUS_ERROR, DOLL_DETAILS, DOLL_DETAILS_ERROR} = require('../../constants');
 events = async (socket) => {
+
+  /* USER */
 
   //Update the socketId of the user
   console.log({New_socket: socket.id})
@@ -86,9 +41,30 @@ events = async (socket) => {
   //   const updateAcolite = await User.patchUser(email, data);
   //   socket.emit('disconnected', updateAcolite);
 
->>>>>>> 7da30ebcfc3adf7d9a7faac23b09489e753b4268
   // });
   
+
+  /* DOLL */
+  socket.on(MISSION_STATUS, async (data) => {
+    try {
+      const updatedDoll = Doll.patchDoll(data.data);
+    } catch(error) {
+      console.log(error);
+      socket.emit(MISSION_STATUS_ERROR, error);
+    }
+  })
+
+  socket.on(DOLL_DETAILS, async (data) => {
+    try {
+      const updatedDoll = Piece.patchPiece(data.pieceName, data.data);
+    } catch(error) {
+      console.log(error);
+      socket.emit(DOLL_DETAILS_ERROR, error);
+    }
+  })
+  
+
+
 }
   
 exports.socketEvents = events;
