9deb27e9fcda0fa9af738b3eefed6821f2697dba
NEREA SARASOLA TOMÉ <nerea.sarasola@ikasle.aeg.eus>
2023-01-24 12:31:38 +0100

    Tarea CC2022-343

---
+++ src/middleWare/jwtValidation.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(!token) {
+        console.log("UNAUTHORIZED")
+        return res.sendStatus(401)
+    }
+ 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, email) => {
+        if(error) {
+            console.log("FORBIDDEN")
+            console.log(error)
+            return res.sendStatus(403)
+        }
+ 
+        req.email = email
+        next()
+    })
+ }
+ 
+exports.authenticateToken = authenticateToken
--- src/routes/dollRoute.js
+++ src/routes/dollRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const dollController = require("../controllers/dollController");
 const router = express.Router();
+const jwt = require("../middleWare/jwtValidation");
 
 router.post("/",dollController.createNewDoll);
-router.get("/",dollController.getDolls);
+router.get("/",jwt.authenticateToken,dollController.getDolls);
 router.patch("/",dollController.patchDoll);
 router.delete("/",dollController.deleteDolls);
 
