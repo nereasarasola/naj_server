489cf1396ed40ff909c168081251ebe1a01c0a22
NEREA SARASOLA TOMÉ <nerea.sarasola@ikasle.aeg.eus>
2023-01-25 10:27:18 +0100

    Cambiado a 15 segundos

--- src/jwt.js
+++ src/jwt.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 const generateAccessToken = (email) => {
    //Una hora: 60 * 60
-   const result = jwt.sign({data: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 })
+   const result = jwt.sign({data: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 15 })
    return result
 }
 
