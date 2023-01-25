e45148e14b5b231239f781125d81882a50ca6c4f
NEREA SARASOLA TOMÉ <nerea.sarasola@ikasle.aeg.eus>
2023-01-25 10:19:49 +0100

    Cambiado la función

--- src/middleWare/jwtValidation.js
+++ src/middleWare/jwtValidation.js
@@ -6,27 +6,20 @@ require('dotenv').config();
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
-    
-    if(authHeader) {
-        const token = authHeader && authHeader.split(' ')[1]
-
-        if(!token) {
-            return res.sendStatus(401)
-        }
-
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, email) => {
-
-            if(error) {
-                return res.sendStatus(403);
-            }
-     
-            req.email = email
-            next();
-        })
+    const token = authHeader && authHeader.split(' ')[1]
+    if(!token) {
+        console.log("UNAUTHORIZED")
+        return res.sendStatus(401)
     }
-    
-
-    
- }
  
-exports.authenticateToken = authenticateToken
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, email) => {
+        if(error) {
+            console.log("FORBIDDEN")
+            return res.sendStatus(403)
+        }
+ 
+        req.email = email
+        next()
+    })
+ }
+ exports.authenticateToken = authenticateToken
