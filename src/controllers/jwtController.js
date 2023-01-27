74149a9d97e99ffaebbc64b502a8a63d7ddc5dc2
NEREA SARASOLA TOMÉ <nerea.sarasola@ikasle.aeg.eus>
2023-01-27 12:47:22 +0100

    Enviado el token al cliente

---
+++ src/controllers/jwtController.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const {generatePermanentToken} = require("../jwt");
+const {STATUS, MISSING_EMAIL, MESSAGE } = require('../constants');
+
+const createPermanentToken = async (req, res) => {
+
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).send({status: STATUS, data: { error: MISSING_EMAIL }});
+    }
+
+    try {
+        let permanentToken = generatePermanentToken(email);
+        console.log({permanentToken: permanentToken});
+        res.send({ token: permanentToken });
+
+    } catch (error) {
+        res.status(error?.status || 500).send({
+            status: STATUS,
+            message: MESSAGE,
+            data: { error: error?.message || error },
+        });
+    } 
+}
+
+exports.createPermanentToken = createPermanentToken;
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const jwtController = require("../controllers/jwtController");
 const tokenAuth = require("../middleWare/tokenAuth");
 const accesToken = require("../middleWare/jwtAccesToken");
 const refreshToken = require("../middleWare/jwtRefreshToken");
-const permanent = require("../jwt");
 const router = express.Router();
 
 router.get("/", accesToken.authenticateToken,userController.getActiveAcolites);
 router.get("/:email",accesToken.authenticateToken, userController.getUserByEmail);
 router.post("/refresh", refreshToken.authenticateRefreshToken);
-router.post("/permanentToken", permanent.generatePermanentToken)
+router.post("/permanentToken", jwtController.createPermanentToken);
 router.post("/", tokenAuth.verifyIdToken, userController.createNewUser);
 router.patch("/cryptEntry/:email",userController.cryptEntry);
 router.patch("/update/:email",userController.patchUser);
