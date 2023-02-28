f369f18170f844a4b798189533c0b148cd9845d1
NEREA SARASOLA TOMÉ <nerea.sarasola@ikasle.aeg.eus>
2023-02-28 10:23:09 +0100

    Conflictos resueltos

--- src/jwt.js
+++ src/jwt.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 const generateAccessToken = (email) => {
    //Una hora: 60 * 60
-   const result = jwt.sign({data: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m'  })
+   const result = jwt.sign({data: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m'})
    return result
 }
 
